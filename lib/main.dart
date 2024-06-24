a3fdedd5214c87cc781540380943609479c6ddce
Sufi Aurangzeb Hossain <azhossain.98.ah@gmail.com>
2024-06-25 00:54:36 +0600

    Modify the auth_repository class and add the auth_remote_data_source interface

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
-import 'package:blog_app/features/auth/presentation/pages/signup_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env").then((value) async {
+    final supabase = await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL']!,
+      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    );
+    debugPrint('supabse: ${supabase.toString()}');
+  });
   runApp(const MyApp());
 }
 
