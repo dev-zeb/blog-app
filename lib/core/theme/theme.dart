63ceaf0c9756c974054302aef4732bb14af41ef7
Sufi Aurangzeb Hossain <azhossain.98.ah@gmail.com>
2024-07-14 19:01:57 +0600

    Modify the theme and add bloc for blogs in the main file

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -3,21 +3,26 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static _border([Color color = AppPallete.borderColor]) => OutlineInputBorder(
-    borderRadius: BorderRadius.circular(10),
-    borderSide: BorderSide(
-      color: color,
-      width: 3,
-    )
-  );
+      borderRadius: BorderRadius.circular(10),
+      borderSide: BorderSide(
+        color: color,
+        width: 3,
+      ));
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: AppPallete.backgroundColor,
     appBarTheme: const AppBarTheme(
       backgroundColor: AppPallete.backgroundColor,
     ),
+    chipTheme: const ChipThemeData(
+      color: MaterialStatePropertyAll(AppPallete.backgroundColor),
+      side: BorderSide.none,
+    ),
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: const EdgeInsets.all(28),
+      border: _border(),
       enabledBorder: _border(),
+      errorBorder: _border(AppPallete.errorColor),
       focusedBorder: _border(AppPallete.gradient1),
-    )
+    ),
   );
 }
